d50dd0312d2ca02f75d7b99c3756ea2da75d4d59
shion32 <annysofiamontano@gmail.com>
2024-08-15 17:13:21 -0500

    agregue el ejercisio dos

---
+++ Ejercisios/Ejercicio2.js
@@ -0,0 +1,32 @@
+
+
+let contador=0
+
+// const nombre = (palabra) =>{
+//     for (var i = 0; i < palabra.length; i++) {
+//         contador+=1
+//         console.log(contador, " ", palabra[i]);
+        
+//     }
+// }
+
+// palabra="Anny";
+
+// nombre(palabra)
+
+let i=0;
+
+const nombre = (palabra) =>{
+   while (i < palabra.length) {
+    contador+=1
+    console.log(contador, " ", palabra[i]);
+    i++
+   }
+}
+
+palabra="Montaño";
+
+nombre(palabra)
+
+
+
