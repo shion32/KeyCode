f3f11f46957061191eb64040422f0c347ad0bd00
shion32 <annysofiamontano@gmail.com>
2024-08-14 20:01:52 -0500

    agregue espacios entre las tablas

--- Ejercisios/ejercio1.js
+++ Ejercisios/ejercio1.js
@@ -6,10 +6,12 @@ let fin=10
  const tabla = () => {
     for (let i = 1; i <= fin; i++) {
       console.log(`la tabla de multiplicar es ${i}`);
+      console.log(" ")
         for (let r =1; r<= 10; r++){
         respuesta=i*r
         console.log(i,"*",r,"=",respuesta)
         }
+      console.log(" ")
       }
 }
  tabla()
