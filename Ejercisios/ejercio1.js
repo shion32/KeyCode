baa4dc32b110525a45a5744f59ee47b21640a63a
shion32 <annysofiamontano@gmail.com>
2024-08-14 19:22:05 -0500

    agrege la carpeta ejercisios y apuntes

---
+++ Ejercisios/ejercio1.js
@@ -0,0 +1,16 @@
+
+respuesta=0
+r=1
+let fin=10
+
+ const tabla = () => {
+    for (let i = 1; i <= fin; i++) {
+      console.log(`la tabla de multiplicar es ${i}`);
+        for (let r =1; r<= 10; r++){
+        respuesta=i*r
+        console.log(i,"*",r,"=",respuesta)
+        }
+      }
+}
+ tabla()
+ 
---
+++ apuntes/variables.js
@@ -0,0 +1,38 @@
+let edadUsuario=25;
+edadUsuario=26;
+const nombreUsuario="Maria"; //variable que no puede cambiar 
+
+//condicionales 
+const edadMinima=18
+if (condicion){
+const usuario={"nombre": "nombre", "edad": 17}
+    console.log("puedes entrar a la discoteca : ")
+}
+
+
+const bandera = true;
+bandera ? "viva colombia" : "ya no vive en colombia"; // es un if else
+!bandera ? "viva colombia" : "ya no vive en colombia"; // es una negacion 
+const pais ="colombia";
+const fruta= "mango";
+bandera && pais == "colombia" ? crossOriginIsolated.log("viva colombia") : conosola.log("ya no vive en colombia")
+(bandera && pais === "colombia") ?  console.log("viva colombia") : conosola.log("ya no vive en colombia")
+//typeof saber que tipo de variable 
+//trabajar en estricto
+
+const totalPares= 0
+const totalImpares= 0
+constInicio= 1
+const fin=20
+
+
+for (let i = inicio; i <= fin; i++) {
+    if (i % 2 == 0){
+        totalPares++
+        console.log("el numero ${i} es par ")
+    } else{
+        totalImpares++;
+        console.log()
+    }
+
+}
